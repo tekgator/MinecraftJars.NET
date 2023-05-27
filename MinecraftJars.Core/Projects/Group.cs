5ac9a05d399c52348210489f27801ef0469e14aa
tekgator <info@tekgator.com>
2023-05-27 17:20:58 +1000

    Remove JsonStrinEnumConverter

--- MinecraftJars.Core/Projects/Group.cs
+++ MinecraftJars.Core/Projects/Group.cs
@@ -1,8 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿namespace MinecraftJars.Core.Projects;
 
-namespace MinecraftJars.Core.Projects;
-
-[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Group
 {
     Server,
