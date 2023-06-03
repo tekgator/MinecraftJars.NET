d6032c1968a6344ba44b791a7fe8124f2d99def4
tekgator <info@tekgator.com>
2023-06-03 12:34:22 +1000

    Write progress message

--- MinecraftJars.Tests/DependencyInjectionTests.cs
+++ MinecraftJars.Tests/DependencyInjectionTests.cs
@@ -11,5 +11,8 @@ public class DependencyInjectionTests
     {
         using var serviceProvider = new ServiceCollection().AddMinecraftJar().BuildServiceProvider();
         Assert.That(serviceProvider.GetService<IMinecraftJar>(), Is.Not.Null);
+        
+        TestContext.Progress.WriteLine("{0}: Injected {1} successful", 
+            nameof(DependencyInjection_Success), nameof(IMinecraftJar));
     } 
 }
